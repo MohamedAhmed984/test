1097ea8c65d93b6e2f1dcfa74dd3b2081246ba12
MohamedAhmed984 <mohamed.eldebes@gmail.com>
2022-04-12 18:09:10 -0500

    edit model

--- app/Models/Blood_type_client.php
+++ app/Models/Blood_type_client.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Blood_type_client extends Model 
 {
 
-    protected $table = 'blood_type_client';
+    // protected $table = 'blood_type_client';
     public $timestamps = true;
 
 }
